4996a17c413dd687f0f177f3d06044362aa6c1c4
JHGondori <JHGondori@github.com>
2025-02-09 17:54:00 +0900

    add gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/bjh/Documents/컴퓨터 프로그래밍/N-body simulator
-# Build directory: /Users/bjh/Documents/컴퓨터 프로그래밍/N-body simulator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(NumberClassTest "/Users/bjh/Documents/컴퓨터 프로그래밍/N-body simulator/build/NumberClassTest")
-set_tests_properties(NumberClassTest PROPERTIES  _BACKTRACE_TRIPLES "/Users/bjh/Documents/컴퓨터 프로그래밍/N-body simulator/CMakeLists.txt;23;add_test;/Users/bjh/Documents/컴퓨터 프로그래밍/N-body simulator/CMakeLists.txt;0;")
